310c8e3d52f63946296c9d3bb0e6662359b052f3
Ashish Bamniya <ashishbam07@gmail.com>
2026-08-15 18:06:13 +0530

    feat add: - Highlight typed characters correct (white) or incorrect (red) in real time - Support backspace to revert character highlighting - Start timer only when user begins typing (not on page load) - Automatically stop timer when countdown reaches zero

--- userinterface.py
+++ userinterface.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter.font import Font
 
-
 WINDOW_WIDTH = 1080
 WINDOW_HEIGHT = 600
 
@@ -9,11 +8,9 @@ TEXT_COLOR = "#666666"
 BACKGROUND_COLOR = "#1E1E1E"
 BUTTON_COLOR = "#FFC000"
 
-WORD_INDEX = 0
 word_fill_index = 0
 fg_color_index = 0
-TIMER = None
-
+START_TIMER = 0
 
 class UserInterface(Tk):
 
@@ -32,6 +29,7 @@ class UserInterface(Tk):
         self.resizable(False, False)
         self.title("typing-speed-tester")
         self.app_font = Font(family="Segoe UI", size=16, slant="italic")
+        self.timer_start_status = False
 
     # ********************************************** TYPING TEST SCREEN ************************************************
 
@@ -80,33 +78,33 @@ class UserInterface(Tk):
         normal_radio_btn.place(x=300, y=470)
         hard_radio_btn.place(x=410, y=470)
 
+        # Create a shared Tkinter variable
+        self.timer_mode = StringVar(self, value="Option 1")
+
         #  Create the Radiobutton widgets
-        second_60_btn = Radiobutton(self, text="60", variable=selected_option, value="Option 1",
-                                    bg=BACKGROUND_COLOR, fg=BUTTON_COLOR, font=("Arial"),
-                                    activebackground=BACKGROUND_COLOR,
-                                    activeforeground="white", selectcolor=BACKGROUND_COLOR,
-                                    command=lambda: self.timer_countdown(60, "60_seconds")
-                                    )
+        self.second_60_btn = Radiobutton(self, text="60", variable=self.timer_mode, value="60, 60_seconds",
+                                         bg=BACKGROUND_COLOR, fg=BUTTON_COLOR, font=("Arial"),
+                                         activebackground=BACKGROUND_COLOR,
+                                         activeforeground="white", selectcolor=BACKGROUND_COLOR,
+                                         )
 
-        second_120_btn = Radiobutton(self, text="120", variable=selected_option, value="Option 2",
-                                     bg=BACKGROUND_COLOR, fg=BUTTON_COLOR, font=("Arial",),
-                                     activebackground=BACKGROUND_COLOR,
-                                     activeforeground="white", selectcolor=BACKGROUND_COLOR,
-                                     command=lambda: self.timer_countdown(120, "120_seconds")
-                                     )
+        self.second_120_btn = Radiobutton(self, text="120", variable=self.timer_mode, value="120, 120_seconds",
+                                          bg=BACKGROUND_COLOR, fg=BUTTON_COLOR, font=("Arial",),
+                                          activebackground=BACKGROUND_COLOR,
+                                          activeforeground="white", selectcolor=BACKGROUND_COLOR,
+                                          )
 
-        second_180_btn = Radiobutton(self, text="180", variable=selected_option,
-                                     value="Option 3",
-                                     bg=BACKGROUND_COLOR, fg=BUTTON_COLOR, font=("Arial"),
-                                     activebackground=BACKGROUND_COLOR,
-                                     activeforeground="white", selectcolor=BACKGROUND_COLOR,
-                                     command=lambda: self.timer_countdown(180, "180_seconds")
-                                     )
+        self.second_180_btn = Radiobutton(self, text="180", variable=self.timer_mode,
+                                          value="180, 180_seconds",
+                                          bg=BACKGROUND_COLOR, fg=BUTTON_COLOR, font=("Arial"),
+                                          activebackground=BACKGROUND_COLOR,
+                                          activeforeground="white", selectcolor=BACKGROUND_COLOR,
+                                          )
 
         # 3. Display the radio button on the screen
-        second_60_btn.place(x=620, y=470)
-        second_120_btn.place(x=715, y=470)
-        second_180_btn.place(x=820, y=470)
+        self.second_60_btn.place(x=620, y=470)
+        self.second_120_btn.place(x=715, y=470)
+        self.second_180_btn.place(x=820, y=470)
 
         # entry field
         self.entered_text_input = Text(self, font=("Arial", 30), width=10, height=1, relief="flat")
@@ -128,17 +126,51 @@ class UserInterface(Tk):
         self.display_text.insert("1.0", typing_text)
         self.display_text.place(x=145, y=120)
 
+
+
     def check_retrieved_text(self, event):
         """this method will check user entered text vs typing text and highlight character with white color if correct and red if wrong character"""
 
-        global fg_color_index
+        global fg_color_index,START_TIMER
 
         # gets text input and gets display typing text display text.
         text_input = self.entered_text_input.get("1.0", "end-1c")
         display_text_string = self.display_text.get("1.0", "end-1c")
 
+        if START_TIMER == 0:
+            timer_mode = self.timer_mode.get().split()
+
+
+            try:
+                total_seconds = int(timer_mode[0].replace(",", ""))
+                selected_timer_option = timer_mode[1]
+
+                self.timer_countdown(total_seconds, selected_timer_option)
+
+            except ValueError:
+
+                total_default_seconds = 60
+                default_timer_option = "60_seconds"
+                self.timer_countdown(total_default_seconds, default_timer_option)
+
+
         # ignores the shift Left side btn while using
-        if event.keysym in ("Shift_L"):
+        if event.keysym == ("Shift_L"):
+            return
+
+        # when user press backspace this if statement will help in backwords to correct a sentence.
+        if event.keysym == ("BackSpace"):
+            fg_color_index -= 1
+            tag_name = f"char_{fg_color_index}"
+
+            # here we are getting the fg_color of the character which needs to be corrected
+            fg_color = self.get_foreground_at_index(self.display_text, f"1.{fg_color_index}")
+
+            # character color is white and red it will reassign that character color to original text color which will help user to navigate at which position he is currently in.
+            if fg_color == "white" or fg_color == "red":
+                self.display_text.tag_config(tag_name, foreground=TEXT_COLOR)
+                self.display_text.tag_add(tag_name, f"1.{fg_color_index}")
+
             return
 
         # this method will give the current text color so that it will update the text color if not filled gives white color, if filled with red & white color than gives red & white color
@@ -148,20 +180,21 @@ class UserInterface(Tk):
         for char_index, (display_char, input_char) in enumerate(zip(display_text_string, text_input)):
 
             if fg_color == TEXT_COLOR:
+
                 tag_name = f"char_{char_index}"  # unique tag per position
 
                 if display_text_string[char_index] == input_char:
                     self.display_text.tag_config(tag_name, foreground="white")
-                    fg_color_index += 1
 
                 elif display_text_string[char_index] != input_char:
-                    self.display_text.tag_config(tag_name, foreground="red")
-                    fg_color_index += 1
 
+                    self.display_text.tag_config(tag_name, foreground="red")
                 self.display_text.tag_add(tag_name, f"1.{char_index}")
 
-            else:
-                pass
+
+
+        fg_color_index += 1
+        START_TIMER += 1
 
     def get_foreground_at_index(self, text_widget, index):
         """this method will give us the current text character of typing text, text color for checking."""
@@ -180,8 +213,9 @@ class UserInterface(Tk):
 
     def timer_countdown(self, seconds_left, selected_timer_option):
 
-        global TIMER,count_second
-        self.timier_text.config(text = seconds_left)
+
+        global count_second, TIMER_START_STATUS
+        self.timier_text.config(text=seconds_left)
         self.timier_text.place(x=510, y=70)
 
 
@@ -192,13 +226,15 @@ class UserInterface(Tk):
         elif selected_timer_option == "180_seconds":
             count_second = seconds_left % 180
 
-
         # this if statement will help in bypassing the issue of when seconds are left less than 10 so it will show then as an 09 08 and so on
         if count_second == 0:
 
             count_second = seconds_left
             if count_second == 0:
                 self.timier_text.place_forget()
+                self.entered_text_input.config(state="disabled")
+
+
                 return
 
         elif count_second < 10:
@@ -208,10 +244,4 @@ class UserInterface(Tk):
 
         # if seconds will be greater than 0 than .after() method will call the time_countdown function again & also subtract the second each second.
         if seconds_left > 0:
-            global timer
-            timer = self.after(300, self.timer_countdown, seconds_left - 1, selected_timer_option)
-
-
-
-
-
+            self.after(500, self.timer_countdown, seconds_left - 1, selected_timer_option)
